5260509ea0eab2ef3ce10df2ceaa6e18ae6d9ccc
anand <ultimatzanand@gmail.com>
2026-02-17 11:18:58 +0000

    jk

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
+apt install python3-venv unzip ffmpeg
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
 wget https://github.com/denoland/deno/releases/download/v2.6.9/deno-x86_64-unknown-linux-gnu.zip
-apt install unzip
 unzip deno-x86_64-unknown-linux-gnu.zip
+rm deno-x86_64-unknown-linux-gnu.zip
 chmod +x deno
 mv deno /usr/local/bin/
 deno --version

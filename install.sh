c540f8ba54c0b4e56e9766c1acb72bf976c74619
anand <ultimatzanand@gmail.com>
2026-02-17 14:51:22 +0530

    df

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ source venv/bin/activate
 pip install -r requirements.txt
 
 wget https://github.com/denoland/deno/releases/download/v2.6.9/deno-x86_64-unknown-linux-gnu.zip
+apt install unzip
 unzip deno-x86_64-unknown-linux-gnu.zip
 chmod +x deno
 mv deno /usr/local/bin/
